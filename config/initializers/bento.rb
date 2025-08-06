7c2190f737b2ce03b6b94b8b70ad1444c1eb677b
Henock L <henockl@live.com>
2025-08-06 18:30:17 +0300

    Add bento settings

--- config/initializers/bento.rb
+++ config/initializers/bento.rb
@@ -1,9 +1,9 @@
 Bento.configure do |config|
   # This is your site's UUID. This scopes all requests.
   # Consider creating a new site for each environment (development and production) in your Bento account.
-  config.site_uuid = "2958eb7112dc2f569e48725aa57d26d5"
+  config.site_uuid = ENV["BENTO_SITE_UUID"]
   # This is your (or another user in your team's) API keys.
   # IMPORTANT: Never store these in your source code as they give full access to your Bento account.
-  config.publishable_key = "pbc26733b41f6e00241db9374a8dcfc6f"
-  config.secret_key = "s72bceafb2c7afe6f8b4c9e0ac9d0464e"
+  config.publishable_key = ENV["BENTO_PUBLISHABLE_KEY"]
+  config.secret_key = ENV["BENTO_SECRET_KEY"]
 end
